4ba624a0cb3a237eb3a780dc5f6aefba82c4754c
Angus-C-git <anguscornall@gmail.com>
2021-08-22 22:38:11 +1000

    ADDED :: quickmaf reversing challnge

--- rev/src/argmultiplexer.c
+++ rev/src/argmultiplexer.c
@@ -1,17 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*
-    Some kind of arg struct
+Argument struct:
+
+    - Example argument foramt:
+        -arg, --argument
+
+    @arg_count:
+        + The number of arguments
+          the argument expects.
+    @quick_tack:
+        + argument short form
+    @full_tack:
+        + argument long form
+    @help
+        + argument help info
 */
+struct arg {
+    int arg_count;
+    char * quick_tack;
+    char * full_tack;
+    char * help;
+};
 
 
 /* parse arguments into struct */
+struct arg new_arg() {
 
+    struct arg new_argument;
+
+    int arg_count;
+    char short_form[6];
+    char long_form[15];
+    char help[42];
+
+    char quick_tack[10] = "-";
+    char full_tack[15] = "--";
+
+
+    printf("Enter expected argument no: ");
+    scanf("%d", &arg_count);
+    getchar();
+
+    if (arg_count <= 0 || arg_count > 3) {
+        printf("Invalid argument count.\n");
+        exit(1);
+    }
+
+    printf("Enter argument short form: ");
+    fgets(short_form, 6, stdin);
+    printf("Enter argument long form: ");
+    fgets(long_form, 15, stdin);
+    printf("Enter argument help info: ");
+    fgets(help, 42, stdin);
+
+    short_form[strlen(short_form) - 1] = '\0';
+    long_form[strlen(long_form) - 1] = '\0';
+    help[strlen(help) - 1] = '\0';  
+
+    strcat(quick_tack, short_form);
+    strcat(full_tack, long_form);
+
+    new_argument.arg_count = arg_count;
+    new_argument.quick_tack = quick_tack;
+    new_argument.full_tack = full_tack;
+    new_argument.help = help;
+    
+    printf("\nGenerated new argument:\n\n");
+    printf("    %s, %s, [%d]        %s", new_argument.quick_tack, 
+                                    new_argument.full_tack, 
+                                    new_argument.arg_count, 
+                                    new_argument.help
+    );
+    printf("\n");
+
+
+    return new_argument;
+}
 
 int 
 main(int argc, char const *argv[])
 {
     /* parse arguments */
-
+    struct arg argument = new_arg();
 
     return 0;
 }
--- rev/src/quickmafs.c
+++ rev/src/quickmafs.c
@@ -1,16 +1,86 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 /*
-
 + A poor mans calculator in C
 
++ BODMAS is not here
 */
 
+#define RUN 1
 
+
+void
+quickmafs()
+{
+    const char operators[4][2] = {
+        "*",
+        "+",
+        "-",
+        "/"
+    };
+
+	char expression[20];
+	char * savepoint;
+
+	int res;
+	int left_operand;
+	int right_operand;
+	char *operator;
+
+
+	char *token;
+	while (1) {
+        printf("<<< ");
+		fgets(expression, 20, stdin);
+        expression[strlen(expression) - 1] = '\0';
+        savepoint = strdup(expression);
+
+		for(int opchar = 0; opchar <= 3; opchar++)
+		{
+
+			token = strtok(expression, operators[opchar]);
+
+			if (strlen(token) == strlen(savepoint))
+				continue;		
+		
+			operator = strdup(operators[opchar]);
+			left_operand = atoi(token);
+			break;
+		}
+
+		token = strtok(NULL, operator);
+		right_operand = atoi(token);
+
+		switch(operator[0])
+		{
+	        case '*':
+	            res = left_operand * right_operand;
+	            break;
+	        case '+':
+	            res = left_operand + right_operand;
+	            break;
+	        case '-':
+	           	res = left_operand - right_operand;
+	            break;
+	        case '/':
+	    	    res = left_operand / right_operand;
+	            break;
+	        default:
+	            printf("Computer says 'no' \n");
+	            res = 0;
+	            break;
+		}
+
+		printf(">>> %d\n", res);
+	}
+}
 
 
 void
 main(void)
 {
-
+    quickmafs();
 }
